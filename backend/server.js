ab353aca76fdc608937305bcd70e3ac1b3690d0d
Tanveer1907 <1906tanveer@gmail.com>
2026-03-04 21:23:23 +0530

    mongodb connection attempt-1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+require("dns").setDefaultResultOrder("ipv4first");
 require('dotenv').config();
 const app = require('./src/app');
-const connectDB = require('./src/config/db');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
